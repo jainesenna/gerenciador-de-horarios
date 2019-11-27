b7a3ade200f66f81eb4efa6b290737e1e6c56400
Pedrojonassm <Pedrojonassm@gmail.com>
2019-11-27 11:15:57 -0300

    adicionado banco de dados no models

--- Projetos/models.py
+++ Projetos/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Materia(models.Model):
+    horario = models.CharField(max_length=22, blank=True, null=True)
+    nome = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'materia'
+    def __str__(self):
+        return self.nome
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'usuario'
+    def __str__(self):
+        return self.nome
+    def __id__(self):
+        return self.id
+
+class Gradeestudo(models.Model):
+    aluno = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='aluno', primary_key=True)
+    materias = models.ForeignKey('Materia', models.DO_NOTHING, db_column='materias')
+
+    class Meta:
+        db_table = 'gradeestudo'
+        unique_together = (('aluno', 'materias'),)
+
+class Atividade(models.Model):
+    aluno = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='aluno')
+    materia = models.ForeignKey('Materia', models.DO_NOTHING, db_column='materia', blank=True, null=True)
+    conteudo = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'atividade'
+    def __str__(self):
+        return self.conteudo
