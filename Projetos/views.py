3d8c15a2d79736d4499c0fb0452536660b35eb8c
Pedrojonassm <Pedrojonassm@gmail.com>
2019-12-01 08:12:54 -0300

    #8 modificado auto-preenchimento de horários de estudos para quem quiser estudar antes, e para quem quiser revisar os asuntos das aulas depois

--- Projetos/views.py
+++ Projetos/views.py
@@ -41,25 +41,42 @@ def Principal(request):
 
 def horarios_auto(request):
     usuario = Usuario.objects.get(id=1)
-
+    try:
+        choice = request.GET["escolha"] # estudar ou revisar
+    except:
+        choice = ""
     estudos = Horarioestudo.objects.filter(aluno = usuario)
     materias = Materia.objects.filter(id__in=Gradeestudo.objects.filter(aluno=usuario).values_list('materias')).order_by('horario')
     sla = ''
     k = 0
     materias_sem_h = Materia.objects.exclude(id__in=Horarioestudo.objects.filter(aluno=usuario).values_list('materias'))
-    for x in ['1','2','3','4','5','6']:
-       dia = Materia.objects.filter(id__in=Gradeestudo.objects.filter(aluno=usuario).values_list('materias'), horario__startswith = x).order_by('horario') # separando as matérias por dia
-       if len(dia) > 0 and dia[k] in materias_sem_h: #verifica se
-           print("X:" + x + "\n")
-           for y in ["12","34","56"]:
-               if len(Horarioestudo.objects.filter(aluno=usuario, horario = x+"M"+y)) == 0: #verifica se ja não existe nada naquele horário
-                   q = Horarioestudo(aluno=usuario, materias=dia[k], horario = x+"M"+y) # criando novo horário de estudo
-                   q.save()
-                   print("Salvo: " + usuario.nome +"  "+ dia[k].nome +"  "+ "horario:  "+x+"M"+y)
-                   k+=1
-               if k>len(dia)-1:
-                   k = 0
-                   break
+    if choice == "estudar":
+        for x in ['1','2','3','4','5','6']:
+            dia = Materia.objects.filter(id__in=Gradeestudo.objects.filter(aluno=usuario).values_list('materias'), horario__startswith = x).order_by('horario') # separando as matérias por dia
+            if len(dia) > 0 and dia[k] in materias_sem_h: #verifica se
+                print("X:" + x + "\n")
+                for y in ["12","34","56"]:
+                    if len(Horarioestudo.objects.filter(aluno=usuario, horario = x+"M"+y)) == 0: #verifica se ja não existe nada naquele horário
+                        q = Horarioestudo(aluno=usuario, materias=dia[k], horario = x+"M"+y) # criando novo horário de estudo
+                        q.save()
+                        k+=1
+                    if k>len(dia)-1:
+                        k = 0
+                        break
+    elif choice == "revisar":
+        for x in ['1','2','3','4','5','6']:
+            dia = Materia.objects.filter(id__in=Gradeestudo.objects.filter(aluno=usuario).values_list('materias'), horario__startswith = x).order_by('horario') # separando as matérias por dia
+            if len(dia) > 0 and dia[k] in materias_sem_h: #verifica se
+                for y in ["12","34","56"]:
+                    if len(Horarioestudo.objects.filter(aluno=usuario, horario = str(int(x)+1)+"M"+y)) == 0: #verifica se ja não existe nada naquele horário
+                        q = Horarioestudo(aluno=usuario, materias=dia[k], horario = str(int(x)+1)+"M"+y) # criando novo horário de estudo
+                        q.save()
+                        k+=1
+                    if k>len(dia)-1:
+                        k = 0
+                        break
+    else:
+        erro = "Você não fez uma escolha"
     return Principal(request)
 
 def horarios_manual(request):
