6dcc6fb4aa5ff6185c4529ed9bec0fe4026a5c6d
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2019-12-03 01:08:12 -0300

    adicionando tela de login

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -76,17 +76,12 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {  
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'trabalhobd2019_2',
-        'USER': 'batman',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'trabalhobd2019_2'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -125,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/Projetos/'
+LOGOUT_REDIRECT_URL = '/account/login'
